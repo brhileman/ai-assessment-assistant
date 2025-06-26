76951210efbed309a302c2fb8e508f90a1ec94dd
Brett <brett@launchpadlab.com>
2025-06-25 21:07:50 -0600

    Fix SendGrid sender verification error
    Change AssessmentMailer from address to brett@launchpadlab.com to match verified sender

--- code/ai_assessment_assistant/app/mailers/assessment_mailer.rb
+++ code/ai_assessment_assistant/app/mailers/assessment_mailer.rb
@@ -1,5 +1,5 @@
 class AssessmentMailer < ApplicationMailer
-  default from: 'AI Assessment Team <assessment@launchpadlab.com>',
+  default from: 'AI Assessment Team <brett@launchpadlab.com>',
           reply_to: 'assessment@launchpadlab.com'
 
   # Subject can be set in your I18n file at config/locales/en.yml
